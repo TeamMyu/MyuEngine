ffe2ecdae970ec6135d280dbc57075f29c297faf
melemo2 <sonmingi0123@gmail.com>
2022-03-10 02:16:35 +0900

    add VulkanUtiles Class

---
+++ src/VulkanUtiles.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "vulkan/vulkan.h"
+#include "GLFW/glfw3.h"
+
+#include <vector>
+#include <set>
+#include <optional>
+#include <iostream>
+
+namespace VulkanUtiles
+{
+    static const std::vector<const char*> m_ValidationLayers = { "VK_LAYER_KHRONOS_validation" };
+    static const std::vector<const char*> m_DeviceExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+
+    struct QueueFamilyIndices {
+        std::optional<uint32_t> graphicsFamily;
+        std::optional<uint32_t> presentFamily;
+
+        bool isComplete() {
+            return graphicsFamily.has_value() && presentFamily.has_value();
+        }
+    };
+
+    struct SwapChainSupportDetails {
+        VkSurfaceCapabilitiesKHR capabilities;
+        std::vector<VkSurfaceFormatKHR> formats;
+        std::vector<VkPresentModeKHR> presentModes;
+    };
+
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device, VkSurfaceKHR surface);
+
+    SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device, VkSurfaceKHR surface);
+
+    void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator);
+
+    VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData);
+
+    void popDebugCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+}
