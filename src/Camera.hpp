f525ef53e68defe6a16975b1b636372182914c05
byunguk.choi <qowlz45@gmail.com>
2022-04-08 18:13:27 +0900

    VUlkanCamera -> Camera

---
+++ src/Camera.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
+namespace Myu {
+
+class Camera{
+    public:
+    void setOrthographicProjection(float left, float right, float top, float bottom, float near, float far);
+    
+    void setPerspectiveProjection(float fovy, float aspect, float near, float far);
+    
+    void setViewDirection(glm::vec3 pos, glm::vec3 dir, glm::vec3 up = glm::vec3(0.0f, -1.0f, 0.0f));
+    
+    void setViewTarget(glm::vec3 pos, glm::vec3 target, glm::vec3 up = glm::vec3(0.0f, -1.0f, 0.0f));
+    
+    void setViewEulerAngle(glm::vec3 pos, glm::vec3 rot);
+    
+    const glm::mat4 getProjectionMatrix() const { return projectionMatrix; }
+    
+    const glm::mat4 getViewMatrix() const { return viewMatrix; }
+    
+    glm::mat4 projectionMatrix{1.0f};
+
+    glm::mat4 viewMatrix{1.0f};
+    
+    private:
+    };
+}
+
+
--- src/VulkanCamera.cpp
+++
@@ -1,5 +0,0 @@
-#include "VulkanCamera.hpp"
-
-namespace VulkanWrapper
-{
-}
