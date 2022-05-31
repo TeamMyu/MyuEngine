7689e366561ce4e3e454bd231d15f1e763bfd891
mingi <melemo2@naver.com>
2022-05-31 21:30:53 +0900

    add VulkanTexture Specification

--- Myu/src/VulkanWrapper/VulkanTexture.cpp
+++ Myu/src/VulkanWrapper/VulkanTexture.cpp
@@ -11,6 +11,25 @@
 
 namespace Myu::VulkanWrapper
 {
+    VulkanTexture::VulkanTexture()
+    {
+        mSpec.samplerInfo.sType                  = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+        mSpec.samplerInfo.magFilter               = VK_FILTER_LINEAR;
+        mSpec.samplerInfo.minFilter               = VK_FILTER_LINEAR;
+        mSpec.samplerInfo.addressModeU            = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+        mSpec.samplerInfo.addressModeV            = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+        mSpec.samplerInfo.addressModeW            = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+        mSpec.samplerInfo.anisotropyEnable        = VK_TRUE;
+        mSpec.samplerInfo.borderColor             = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
+        mSpec.samplerInfo.unnormalizedCoordinates = VK_FALSE;
+        mSpec.samplerInfo.compareEnable           = VK_FALSE;
+        mSpec.samplerInfo.compareOp               = VK_COMPARE_OP_ALWAYS;
+        mSpec.samplerInfo.mipmapMode              = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+
+        mSpec.imageUsageFlags = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
+        mSpec.imageLayout     = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+    }
+ 
     void VulkanTexture::destroy()
     {
         vkDestroyImageView(mDevice->GetVkLogicalDevice(), mImageView, nullptr);
@@ -22,12 +41,35 @@ namespace Myu::VulkanWrapper
         vkFreeMemory(mDevice->GetVkLogicalDevice(), mImageMemory, nullptr);
     }
 
+    void VulkanTexture::createTextureTarget(VulkanDevice *device, uint32_t w, uint32_t h, VkFormat format)
+    {
+        this->mDevice = device;
+        this->width   = w;
+        this->height  = h;
+
+        createImage(mDevice->GetVkPhysicalDevice(), mDevice->GetVkLogicalDevice(), w, h, format, VK_IMAGE_TILING_OPTIMAL, mSpec.imageUsageFlags, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, mImage, mImageMemory);
+
+        Utils::transitionImageLayout(*mDevice, mImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_UNDEFINED, mSpec.imageLayout);
+        Utils::createImageView(device->GetVkLogicalDevice(), mImage, &mImageView, format, VK_IMAGE_ASPECT_COLOR_BIT);
+
+        // get maxSamplerAnisotropy for sampler
+        VkPhysicalDeviceProperties properties{};
+        vkGetPhysicalDeviceProperties(device->GetVkPhysicalDevice(), &properties);
+
+        mSpec.samplerInfo.maxAnisotropy = properties.limits.maxSamplerAnisotropy;
+
+        VK_CHECK_RESULT(vkCreateSampler(device->GetVkLogicalDevice(), &mSpec.samplerInfo, nullptr, &mSampler));
+    }
+
     void VulkanTexture::loadFromFile(VulkanDevice *device, const char *filePath)
     {
         this->mDevice = device;
         
         int texWidth, texHeight, texChannels;
         stbi_uc *pixels = stbi_load(filePath, &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+
+        this->width = texWidth;
+        this->height = texHeight;
 
         if (!pixels)
         {
@@ -53,12 +95,10 @@ namespace Myu::VulkanWrapper
         vkUnmapMemory(mDevice->GetVkLogicalDevice(), stagingBufferMemory);
 
         if (imageSize != 64 || texChannels != 1) stbi_image_free(pixels);
-        
 
         createImage(mDevice->GetVkPhysicalDevice(), mDevice->GetVkLogicalDevice(), texWidth, texHeight,
                     VK_FORMAT_R8G8B8A8_SRGB,
-                    VK_IMAGE_TILING_OPTIMAL,
-                    VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+                    VK_IMAGE_TILING_OPTIMAL, mSpec.imageUsageFlags,
                     VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
                     mImage, mImageMemory);
 
@@ -66,7 +106,7 @@ namespace Myu::VulkanWrapper
         
         Utils::copyBufferToImage(*mDevice, stagingBuffer, mImage, TO_UINT32(texWidth), TO_UINT32(texHeight));
         
-        Utils::transitionImageLayout(*mDevice, mImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+        Utils::transitionImageLayout(*mDevice, mImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, mSpec.imageLayout);
 
         vkDestroyBuffer(device->GetVkLogicalDevice(), stagingBuffer, nullptr);
         vkFreeMemory(device->GetVkLogicalDevice(), stagingBufferMemory, nullptr);
@@ -77,21 +117,8 @@ namespace Myu::VulkanWrapper
         VkPhysicalDeviceProperties properties{};
         vkGetPhysicalDeviceProperties(device->GetVkPhysicalDevice(), &properties);
         
-        // create sampler
-        VkSamplerCreateInfo samplerInfo{};
-        samplerInfo.sType                   = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
-        samplerInfo.magFilter               = VK_FILTER_LINEAR;
-        samplerInfo.minFilter               = VK_FILTER_LINEAR;
-        samplerInfo.addressModeU            = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-        samplerInfo.addressModeV            = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-        samplerInfo.addressModeW            = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-        samplerInfo.anisotropyEnable        = VK_TRUE;
-        samplerInfo.maxAnisotropy           = properties.limits.maxSamplerAnisotropy;
-        samplerInfo.borderColor             = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
-        samplerInfo.unnormalizedCoordinates = VK_FALSE;
-        samplerInfo.compareEnable           = VK_FALSE;
-        samplerInfo.compareOp               = VK_COMPARE_OP_ALWAYS;
-        samplerInfo.mipmapMode              = VK_SAMPLER_MIPMAP_MODE_LINEAR;
-        VK_CHECK_RESULT(vkCreateSampler(device->GetVkLogicalDevice(), &samplerInfo, nullptr, &mSampler));
+        mSpec.samplerInfo.maxAnisotropy           = properties.limits.maxSamplerAnisotropy;
+
+        VK_CHECK_RESULT(vkCreateSampler(device->GetVkLogicalDevice(), &mSpec.samplerInfo, nullptr, &mSampler));
     }
 }
--- Myu/src/VulkanWrapper/VulkanTexture.hpp
+++ Myu/src/VulkanWrapper/VulkanTexture.hpp
@@ -5,14 +5,28 @@
 
 namespace Myu::VulkanWrapper
 {
+    struct VulkanTextureSpec
+    {
+        VkSamplerCreateInfo samplerInfo{};
+        VkImageUsageFlags   imageUsageFlags;
+        VkImageLayout imageLayout;
+    };
     class VulkanTexture
     {
     public:
+        VulkanTexture();
+
         void destroy();
         void loadFromFile(VulkanDevice* device, const char * filePath);
+        void createTextureTarget(VulkanDevice* device, uint32_t width, uint32_t height, VkFormat format);
+
         VkImageView& getImageView() {return mImageView;}
         VkSampler& getSampler() {return mSampler;}
         
+        VulkanTextureSpec mSpec;
+        uint32_t          width;
+        uint32_t          height;
+
     private:
         VulkanDevice *mDevice;
         VkImage mImage;
