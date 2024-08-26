f1f79da531322869260a9bf7c2e990c09668efd4
melemo2 <sonmingi0123@gmail.com>
2024-08-26 14:58:53 +0900

    update

--- server/run.sh
+++ server/run.sh
@@ -1,15 +1,24 @@
 git clone https://github.com/lllyasviel/stable-diffusion-webui-forge
-apt install python3.10-venv -y
-python3.10 -m venv venv
-source ./venv/bin/activate
-./venv/bin/pip3 install -r requirements.txt
 
 OS_TYPE=$(uname -s)
 
 if [ "$OS_TYPE" = "Darwin" ]; then
     echo "This is macOS."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install python@3.10
+    
+    python3.10 -m venv venv
+    source ./venv/bin/activate
+    ./venv/bin/pip3 install -r requirements.txt
+    
     sh ./stable-diffusion-webui-forge/webui.sh --always-cpu --api --allow-code --xformers --skip-torch-cuda-test --no-half-vae --precision full --no-half --use-cpu &
+
 elif [ "$OS_TYPE" = "Linux" ]; then
+    apt install python3.10-venv -y
+    python3.10 -m venv venv
+    source ./venv/bin/activate
+    ./venv/bin/pip3 install -r requirements.txt
+
     sh ./stable-diffusion-webui-forge/webui.sh --api --allow-code --xformers --skip-torch-cuda-test --no-half-vae --precision full --no-half &
 else
     echo "Unknown operating system: $OS_TYPE"
